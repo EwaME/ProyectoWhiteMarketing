dba24fdb4ac3639ff57a4b2b06c6c14ab450f0d8
Sthefany Sosa <usthm_sosae@unicah.edu>
2025-12-11 03:43:37 -0600

    agg inicio

--- servicesSupreme/img/services/services.js
+++ servicesSupreme/img/services/services.js
@@ -1,11 +1,52 @@
 let logosContainer = document.querySelector('.brands-Logos');
-let  logos = logosContainer.innerHTML; // guarda el contenido original
-logosContainer.innerHTML += logos;      // lo duplica
+let  logos = logosContainer.innerHTML; 
+logosContainer.innerHTML += logos;     
 
-// Opcional: pausar al pasar el mouse
+
 logosContainer.addEventListener('mouseenter', () => {
 logosContainer.style.animationPlayState = 'paused';
 });
 logosContainer.addEventListener('mouseleave', () => {
 logosContainer.style.animationPlayState = 'running';
 });
+
+
+//**inicio botones */
+
+let trackDerecha = document.getElementById('track-derecha');
+        let trackIzquierda = document.getElementById('track-izquierda');
+
+     
+        let filaDerecha = document.querySelector('.galeria-fila-derecha');
+        let filaIzquierda = document.querySelector('.galeria-fila-izquierda');
+
+        let duplicarContenido = (trackElement) => {
+            if (trackElement && trackElement.innerHTML.trim() !== '') {
+                let contenidoOriginal = trackElement.innerHTML;
+            
+                trackElement.innerHTML += contenidoOriginal;
+            }
+        };
+
+    
+        const configurarControl = (filaElement, trackElement) => {
+            if (filaElement && trackElement) {
+              
+                filaElement.addEventListener('mouseenter', () => {
+                    trackElement.style.animationPlayState = 'paused';
+                });
+
+            
+                filaElement.addEventListener('mouseleave', () => {
+                    trackElement.style.animationPlayState = 'running';
+                });
+            }
+        };
+
+
+        duplicarContenido(trackDerecha);
+        configurarControl(filaDerecha, trackDerecha);
+
+
+        duplicarContenido(trackIzquierda);
+        configurarControl(filaIzquierda, trackIzquierda);
